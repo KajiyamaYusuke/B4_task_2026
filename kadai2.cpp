cb826c0725e745fba2845853bfed46d42bce5b50
chihiro1127 <u299790k@ecs.osaka-u.ac.jp>
2026-04-14 15:19:34 +0900

    202604142

--- kadai2.cpp
+++ kadai2.cpp
@@ -1,1 +1,30 @@
-ppp
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void SolveAndSave(string matlixfile, string vectorFile, string outputFile){
+     int N = 100;
+
+     vector<vector<double>> A(N,vector<double>(N));
+     vector<double> b(N);
+
+     ifstream fileA(matrixFile), fileB(vectorFile);
+
+}
+
+     char comma;
+
+     for (int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            fileA >> A[i][j];
+            if (j < N-1) fileA >> comma;
+        }
+        fileB >> b[i];
+     }
+    
+     
+

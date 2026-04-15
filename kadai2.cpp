9f7881104c5bb38e05f5b00c1b5e0a5aae2e6e14
chihiro1127 <u299790k@ecs.osaka-u.ac.jp>
2026-04-15 09:42:45 +0900

    20260415

--- kadai2.cpp
+++ kadai2.cpp
@@ -19,12 +19,29 @@ void SolveAndSave(string matlixfile, string vectorFile, string outputFile){
      char comma;
 
      for (int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
+        for (int j = 0; j < N; j++){
             fileA >> A[i][j];
             if (j < N-1) fileA >> comma;
         }
         fileB >> b[i];
      }
     
+     for (int i=0; i < N; i++){
+        int pivot = i;
+        for (int k=i+1; k < N; k++){
+            if (abs(A[k][i]) > abs(A[pivot][i])) pivot = k;
+        }
      
+     swap(A[i], A[pivot]);
+     swap(b[i], b[pivot]);
 
+     for (int k=i+1; k < N; k++){
+        double f = A[k][i] / A[i][i];
+        
+        for (int j=1; j < N; j++){
+            A[k][j] -= f * A[i][j];
+        }
+        b[k] -= f * b[i];
+     }
+
+    }

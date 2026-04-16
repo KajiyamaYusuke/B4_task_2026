b4e662ad09735e6c39341c0436a61ac5a37f9391
chihiro1127 <u299790k@ecs.osaka-u.ac.jp>
2026-04-16 09:46:43 +0900

    20260416

--- kadai2.cpp
+++ kadai2.cpp
@@ -14,11 +14,9 @@ void SolveAndSave(string matlixfile, string vectorFile, string outputFile){
 
      ifstream fileA(matrixFile), fileB(vectorFile);
 
-}
-
      char comma;
 
-     for (int i=0; i<N; i++){
+     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
             fileA >> A[i][j];
             if (j < N-1) fileA >> comma;
@@ -26,22 +24,40 @@ void SolveAndSave(string matlixfile, string vectorFile, string outputFile){
         fileB >> b[i];
      }
     
-     for (int i=0; i < N; i++){
+     for (int i = 0; i < N; i++){
         int pivot = i;
-        for (int k=i+1; k < N; k++){
+        for (int k= i + 1; k < N; k++){
             if (abs(A[k][i]) > abs(A[pivot][i])) pivot = k;
         }
      
      swap(A[i], A[pivot]);
      swap(b[i], b[pivot]);
 
-     for (int k=i+1; k < N; k++){
+     for (int k = i + 1; k < N; k++){
         double f = A[k][i] / A[i][i];
         
-        for (int j=1; j < N; j++){
+        for (int j = 1; j < N; j++){
             A[k][j] -= f * A[i][j];
         }
         b[k] -= f * b[i];
      }
 
     }
+
+    for (int i = N - 1; i >= 0; i--) {
+        
+        for (int j = i + 1; j < N; j++) { 
+            b[i] -= A[i][j] * b[j]; 
+        }
+        b[i] /= A[i][i]; 
+        
+    }
+
+    ofstream outFile(outputFile);
+    
+    for (int i = 0; i < N; i++) {
+        outFile << b[i] << "\n"; 
+    }
+
+    cout << "処理完了: " << outputFile << " に結果を出力しました。" << endl;
+}
